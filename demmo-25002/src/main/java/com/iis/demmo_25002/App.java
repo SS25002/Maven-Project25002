5fd3964801f2f90c7d6765aeab81d58acf4229c6
Javier <jssphk@outlook.com>
2025-05-11 23:27:48 -0600

    My second commit

---
+++ demmo-25002/src/main/java/com/iis/demmo_25002/App.java
@@ -0,0 +1,16 @@
+package com.iis.demmo_25002;
+
+/**
+ * Hello world!
+ */
+public class App {
+	
+	public String Hello() {
+		return "Hello World";
+	}
+    public static void main(String[] args) {
+    	
+    	App hw = new App();
+        System.out.println(hw.Hello());
+    }
+}
